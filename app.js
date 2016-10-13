0891454d625229c74f05d2b82ea67a04f68fadd6
Advaita Krishna das <advaita.krishna.das@gmail.com>
2016-10-13 16:49:08 +0300

    insert new doc
    + updates «date» field using machine-data: «March 2016» -> [2016, 3]
    + inserts new document to db

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+var events = [
+  { label: "Январь 2016",  type: "monthly", date: [2016, 1] },
+  { label: "Февраль 2016", type: "monthly", date: [2016, 2] },
+  { label: "Март 2016",    type: "monthly", date: [2016, 3] },
+  { label: "Марафон Прабхупады 2016 (Декабрь)", type: "event" },
+];
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
@@ -40,13 +47,6 @@ app.get('/locations.json', function (req, res) {
 app.get('/events.json', function (req, res) {
   var term = req.query.term;
 
-  var events = [
-    { label: "Январь 2016",  type: "monthly", date: [2016, 1] },
-    { label: "Февраль 2016", type: "monthly", date: [2016, 2] },
-    { label: "Март 2016",    type: "monthly", date: [2016, 3] },
-    { label: "Марафон Прабхупады 2016 (Декабрь)", type: "event" },
-  ];
-
   var result = underscore.filter(events, function (item) {
     return item.label.indexOf(term) != -1;
   });
@@ -56,17 +56,14 @@ app.get('/events.json', function (req, res) {
 
 app.post('/api/report/new', function (req, res) {
   var doc = req.body;
-  var reportCheckState = report.check(doc);
+  var reportCheckState = report.check(doc, { events: events });
 
   if (reportCheckState.success) {
-    db.insert(doc, function(err, body) {
-      if (!err)
-        console.log(body);
-      else
-        console.log(err);
-    });
-  } else {
-    console.log(reportCheckState);
+    var realDate = null;
+    var event = underscore.find(events, function (f) { return f.label == doc.date; });
+    doc.date = event.date;
+    doc.type = event.type;
+    db.insert(doc, function(err, body) { });
   }
 
   res.send(reportCheckState);
--- report.js
+++ report.js
@@ -1,29 +1,36 @@
+var underscore = require('underscore');
+
 // Checks report for consistency
-exports.check = function (report) {
+// options = { events: {} }
+exports.check = function (report, options) {
   var errors = [];
+  var eventNames = underscore.map(options.events, function (e) { return e.label; });
 
   function appendError(fld, msg) {
     errors.push({ field: fld, message: msg });
   }
 
   // report must have "locaton" field
-  if (report.location == null) { // TODO: replace with nullOrEmpty
+  if (report.location === null) { // TODO: replace with nullOrEmpty
     appendError("location", "Report must have 'location' field");
   } else {
     if (report.location.length <= 3) { appendError("location", "Location name is to short"); }
     if (report.location.length >= 128) { appendError("location", "Location name to long."); }
     // location name must not have trailing spaces
+    // check location existance
   }
 
   // report must have "data" field
-  if (report.date == null) { // TODO: replace with nullOrEmpty
+  if (report.date === null) { // TODO: replace with nullOrEmpty
     appendError("date", "Report must have 'date' field");
   } else {
-    if (report.date.length <= 0) { appendError("date", "Date name to short."); } // TODO: check date format
+    if (report.date.length <= 0) { appendError("date", "Date name to short."); }
+    if (report.date.length >= 128) { appendError("date", "Date name to long."); }
+    if (!underscore.contains(eventNames, report.date)) { appendError("date", "Event not found."); }
   }
 
   // report must have "records" field
-  if (report.records == null || report.records.length <= 0) { // TODO: replace with nullOrEmptyArray
+  if (report.records === null || report.records.length <= 0) { // TODO: replace with nullOrEmptyArray
     appendError("report", "Report must have 'records' field");
   } else {
     // TODO: check for counts field
@@ -32,7 +39,7 @@ exports.check = function (report) {
 
   // return result
   return {
-    success: errors.length == 0,
+    success: errors.length === 0,
     errors: errors
-  }
-}
+  };
+};
